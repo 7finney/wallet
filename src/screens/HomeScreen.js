501576f158361d71a2be2b3ec2e48622befebed9
Ayan Banerjee <ayanb1999@gmail.com>
2020-04-16 16:15:57 +0530

    Added changes to rendering tx Data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import {StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
-import {Layout, Text, Button, Card, Icon} from '@ui-kitten/components';
+import {StyleSheet, Dimensions, TouchableOpacity, ScrollView} from 'react-native';
+import {Layout, Text, Button, Card, Icon, Input} from '@ui-kitten/components';
 import deployUnsignedTx from '../services/sign';
 import QRScanner from '../components/QRScanner';
 
@@ -45,7 +45,6 @@ const HomeScreen = () => {
   const [scan, setScan] = useState(false);
 
   const handleSignTx = () => {
-    console.log('Handling');
     setError('');
     const {transactionHash, rawTransaction} = deployUnsignedTx(unsignedTx);
     setTxHash(transactionHash);
@@ -65,96 +64,113 @@ const HomeScreen = () => {
     }
   };
   return (
-    <Layout style={styles.container}>
-      <Layout style={styles.homeHeader}>
-        <Text style={styles.homeHeaderText} category="h1">
-          Wallet
-        </Text>
-      </Layout>
-      {error !== '' && (
-        <Layout style={styles.error}>
-          <Text style={{textAlign: 'center', color: '#fff', fontSize: 18}}>{error}</Text>
+    <ScrollView>
+      <Layout style={styles.container}>
+        <Layout style={styles.homeHeader}>
+          <Text style={styles.homeHeaderText} category="h1">
+            Wallet
+          </Text>
         </Layout>
-      )}
-      <Layout
-        style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', padding: 5}}>
-        {Object.keys(unsignedTx).length > 0 && (
-          <Layout
-            style={{
-              marginBottom: 10,
-              padding: 10,
-            }}>
-            <Card>
-              <Text appearance="hint">Transaction To Be Signed: </Text>
-              {Object.keys(unsignedTx).map((k) => (
-                <Text h4 key={k}>
-                  {k}: {unsignedTx[k]}{' '}
-                </Text>
-              ))}
-            </Card>
+        {error !== '' && (
+          <Layout style={styles.error}>
+            <Text style={{textAlign: 'center', color: '#fff', fontSize: 18}}>{error}</Text>
           </Layout>
         )}
-        {txHash.length > 0 && rawTx.length > 0 && (
-          <Layout
-            style={{
-              marginBottom: 10,
-              padding: 10,
-            }}>
-            <Card>
-              <Text appearance="hint">Transaction Hash: </Text>
-              <Text>{txHash}</Text>
-              <Text appearance="hint">Raw Transaction: </Text>
-              <Text> {rawTx}</Text>
-            </Card>
-          </Layout>
-        )}
-        {Object.keys(unsignedTx).length > 0 && (
-          <Layout>
-            {txHash === '' ? (
-              <Button style={styles.signBtn} onPress={handleSignTx}>
-                Sign Transaction
-              </Button>
-            ) : (
-              <Button style={[styles.signBtn, {backgroundColor: '#15348a'}]}>
-                Sign Transaction
-              </Button>
-            )}
-          </Layout>
-        )}
+        <Layout
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            padding: 5,
+          }}>
+          {Object.keys(unsignedTx).length > 0 && (
+            <Layout
+              style={{
+                marginBottom: 10,
+                padding: 10,
+              }}>
+              <Card>
+                <Text appearance="hint">Transaction To Be Signed: </Text>
+                {Object.keys(unsignedTx).map((k) => (
+                  <Layout>
+                    <Text h4 key={k}>
+                      {k}:
+                    </Text>
+                    <Input placeholder="Place your Text" value={unsignedTx[k]} disbaled />
+                  </Layout>
+                ))}
+              </Card>
+            </Layout>
+          )}
+          {txHash.length > 0 && rawTx.length > 0 && (
+            <Layout
+              style={{
+                marginBottom: 10,
+                padding: 10,
+              }}>
+              <Card>
+                <Text appearance="hint">Transaction Hash: </Text>
+                <Text>{txHash}</Text>
+                <Text appearance="hint">Raw Transaction: </Text>
+                <Text> {rawTx}</Text>
+              </Card>
+            </Layout>
+          )}
+          {Object.keys(unsignedTx).length > 0 && (
+            <Layout>
+              {txHash === '' ? (
+                <Layout>
+                  <Button style={styles.signBtn} onPress={handleSignTx}>
+                    Sign Transaction
+                  </Button>
+                </Layout>
+              ) : (
+                <Layout>
+                  <Button style={[styles.signBtn, {backgroundColor: '#15348a'}]}>
+                    Sign Transaction
+                  </Button>
+                  <Button style={styles.signBtn} onPress={handleSignTx}>
+                    Deploy Transaction
+                  </Button>
+                </Layout>
+              )}
+            </Layout>
+          )}
+        </Layout>
+        {scan && <QRScanner onSuccess={handleScanner} />}
+        <TouchableOpacity
+          style={{
+            borderWidth: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 70,
+            position: 'absolute',
+            bottom: 10,
+            right: 10,
+            height: 70,
+            backgroundColor: '#fff',
+            borderRadius: 100,
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: -2},
+            shadowOpacity: 0.5,
+            shadowRadius: 2,
+            elevation: 2,
+          }}
+          onPress={() => {
+            setScan(!scan);
+            setError('');
+            setTxHash('');
+            setrawTx('');
+            setUnsignedTx({});
+          }}>
+          {scan === false ? (
+            <Icon style={styles.icon} fill="#8F9BB3" name="camera" />
+          ) : (
+            <Icon style={styles.icon} fill="#8F9BB3" name="close" />
+          )}
+        </TouchableOpacity>
       </Layout>
-      {scan && <QRScanner onSuccess={handleScanner} />}
-      <TouchableOpacity
-        style={{
-          borderWidth: 0,
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: 70,
-          position: 'absolute',
-          bottom: 10,
-          right: 10,
-          height: 70,
-          backgroundColor: '#fff',
-          borderRadius: 100,
-          shadowColor: '#000',
-          shadowOffset: {width: 0, height: -2},
-          shadowOpacity: 0.5,
-          shadowRadius: 2,
-          elevation: 2,
-        }}
-        onPress={() => {
-          setScan(!scan);
-          setError('');
-          setTxHash('');
-          setrawTx('');
-          setUnsignedTx({});
-        }}>
-        {scan === false ? (
-          <Icon style={styles.icon} fill="#8F9BB3" name="camera" />
-        ) : (
-          <Icon style={styles.icon} fill="#8F9BB3" name="close" />
-        )}
-      </TouchableOpacity>
-    </Layout>
+    </ScrollView>
   );
 };
 
